12f1bcacf6c3019ceca8e113ff1ec6764d267255
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-04-19 20:31:43 -0600

    Create edit and update actions in Bulk Discount controller and ensured percentage_off and minimum_items must be a number

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -7,4 +7,25 @@ class BulkDiscountsController < ApplicationController
   def show
     @bulk_discount = BulkDiscount.find(params[:id])
   end
+
+  def edit
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    @bulk_discount = BulkDiscount.find(params[:id])
+    @bulk_discount.update(discount_params)
+    if @bulk_discount.save
+      flash[:success] = "Your discount has been updated"
+      redirect_to bulk_discount_path(@bulk_discount)
+    else
+      flash[:error] = @bulk_discount.errors.full_messages.to_sentence
+      redirect_to edit_bulk_discount_path(@bulk_discount)
+    end
+  end
+
+  private
+  def discount_params
+    params.require(:bulk_discount).permit(:description, :minimum_items, :percentage_off, :id)
+  end
 end
